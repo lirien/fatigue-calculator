81a5130600b484b21dcb58dbf91fc2d9d54f21ae
Erin Sullivan <kittylily@gmail.com>
2016-11-20 18:08:16 -0800

    🔢 Add triangle number and fatigue calculations

--- src/fatigue-calculator.js
+++ src/fatigue-calculator.js
@@ -1,5 +1,14 @@
 class FatigueCalculator {
+  triangleNumber(n) {
+    return n * (n + 1) / 2;
+  }
 
+  fatigueDamage(cardsRemaining, cardsDrawn) {
+    if(cardsRemaining >= 0) {
+      return this.triangleNumber(cardsDrawn - cardsRemaining);
+    }
+    return this.triangleNumber(cardsDrawn - cardsRemaining) - this.triangleNumber(-cardsRemaining);
+  }
 }
 
 module.exports = FatigueCalculator;
